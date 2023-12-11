5653341603820e42dac0e9b5cc5a44e169239fdf
bogvsz <114946817+bogvszdev@users.noreply.github.com>
2023-12-11 20:02:01 +0100

    Add Semantic UI intruducion

---
+++ app/(auth)/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+
+<head>
+    <!-- Deafult meta -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- CSS Styles -->
+    <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+    <!-- Semantic UI -->
+    <script src="semantic/dist/semantic.min.js"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
